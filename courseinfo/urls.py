e9e8f582ebba085a5dcd09596049571569d072f0
DDM <32198970+DDMerr@users.noreply.github.com>
2022-03-12 11:19:04 -0600

    some changes

--- courseinfo/urls.py
+++ courseinfo/urls.py
@@ -24,33 +24,43 @@ from courseinfo.views import (
 urlpatterns = [
 
 
-    path('instructor/', InstructorList.as_view(), name='courseinfo_instructor_list_urpattern'),
+    path('instructor/',
+         InstructorList.as_view(),
+         name='courseinfo_instructor_list_urlpattern'),
+
     path('instructor/<int:pk>/',
          InstructorDetail.as_view(),
          name='courseinfo_instructor_detail_urlpattern'),
 
-    path('section/', SectionList.as_view(), name='courseinfo_section_list_urpattern'),
+    path('section/',
+         SectionList.as_view(),
+         name='courseinfo_section_list_urlpattern'),
+
     path('section/<int:pk>/',
          SectionDetail.as_view(),
          name='courseinfo_section_detail_urlpattern'),
 
-    path('course/', CourseList.as_view(), name='courseinfo_course_list_urpattern'),
+    path('course/',
+         CourseList.as_view(),
+         name='courseinfo_course_list_urlpattern'),
     path('course/<int:pk>/',
          CourseDetail.as_view(),
          name='courseinfo_course_detail_urlpattern'),
 
-    path('semester/', SemesterList.as_view(), name='courseinfo_semester_list_urpattern'),
+    path('semester/',
+         SemesterList.as_view(),
+         name='courseinfo_semester_list_urlpattern'),
 
     path('semester/<int:pk>/',
          SemesterDetail.as_view(),
-         name='courseinfo_semester_detail_urpattern'),
-    path('student/', StudentList.as_view(), name='courseinfo_student_list_urpattern'),
+         name='courseinfo_semester_detail_urlpattern'),
+    path('student/', StudentList.as_view(), name='courseinfo_student_list_urlpattern'),
     path('student/<int:pk>/',
          StudentDetail.as_view(),
          name='courseinfo_student_detail_urlpattern'),
 
-    path('registration/', RegistrationList.as_view(), name='courseinfo_registration_list_urpattern'),
+    path('registration/', RegistrationList.as_view(), name='courseinfo_registration_list_urlpattern'),
     path('registration/<int:pk>/',
          RegistrationDetail.as_view(),
-         name='courseinfo_registration_detail_urpattern'),
+         name='courseinfo_registration_detail_urlpattern'),
 ]
